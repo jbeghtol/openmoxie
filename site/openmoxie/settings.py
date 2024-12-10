8178c2807641f960c847229eecc0e9a58fa8f861
Justin Beghtol <Justin.Beghtol@embodied.me>
2024-12-10 14:48:08 -0800

    Really removed polls

--- site/openmoxie/settings.py
+++ site/openmoxie/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hive.apps.HiveConfig',
-#    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- site/openmoxie/urls.py
+++ site/openmoxie/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('hive/', include("hive.urls")),
-#    path('polls/', include("polls.urls")),
     path('admin/', admin.site.urls),
 ] + debug_toolbar_urls()
 
