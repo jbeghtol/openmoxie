4f55d1d8601d4ce6244834407b39203a63f4360d
Justin Beghtol <Justin.Beghtol@embodied.me>
2024-12-12 13:46:01 -0800

    Removed invalid setting for port

--- site/openmoxie/settings.py
+++ site/openmoxie/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import os
 from django.conf import settings
 
-settings.RUNSERVER_PORT = 8080
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
